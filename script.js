2795bc30685a940634280a285b0b4184c45149e6
Dhanush <110124614+dhnushreddy@users.noreply.github.com>
2024-11-16 19:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const byteSize = (str) => {
   // write your code here
-	const blobData=new Blob(str);
-	return  blobData.size();
+	const blobData=new Blob([str]);
+	return  blobData.size;
 	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+// const str = prompt("Enter some string.");
 alert(byteSize(str));
